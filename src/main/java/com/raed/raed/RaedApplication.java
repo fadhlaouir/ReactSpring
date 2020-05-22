228db9cf047021f25c93c045e72d3c88a6db92df
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-05-22 19:31:31 +0100

    setup project

---
+++ src/main/java/com/raed/raed/RaedApplication.java
@@ -0,0 +1,13 @@
+package com.raed.raed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raed/raed/RaedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raed.raed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
