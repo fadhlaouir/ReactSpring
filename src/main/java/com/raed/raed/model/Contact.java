9c6e418d4bcb2da85996249abe2274cb662f589e
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-05-23 00:27:02 +0100

    add Contact Model

---
+++ src/main/java/com/raed/raed/model/Contact.java
@@ -0,0 +1,25 @@
+package com.raed.raed.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class Contact {
+    private @Id @GeneratedValue Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private Contact() {
+    }
+
+    public Contact(String firstName, String lasString, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
